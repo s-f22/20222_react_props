15ee354e158284c882276ec7fd53c278363bdb7b
Karamazov <samuel.silveira01@fatec.sp.gov.br>
2022-09-02 21:45:13 -0300

    apostila04: bloco de codigo 2.5.5 concluido

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+
+
+const App = () => {
+    return (
+        <div className="container border mt-2">
+            <div className="row border-bottom">
+                <h1 className="display-5 text-center">Seus pedidos</h1>
+                <div className="row">
+                    <div className="col-sm-8 col-md-6">
+                        <div className="card">
+                            <div className="card-header text-muted">22/04/2021</div>
+                            <div className="card-body d flex">
+                                <div className="d-flex align-items-center">
+                                    <i className="fas fa-hdd fa-2x"></i>
+                                </div>
+                                {/* <div className="flex-grow-1 ms-2 border"></div> */}
+                                <div className="flex-grow-1 ms-2 border">
+                                    <h4 className="text-center">SSD</h4>
+                                    <p className="text-center">SSD Kingston A400 - SATA</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+//encaixa a função App no root do index.html (geral)
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
